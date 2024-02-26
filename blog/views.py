367f972ce101b9106e65f08aaf708c6049b8748b
jpsantostefano <jpsantostefano@gmail.com>
2024-02-26 18:22:19 +0000

    post_detail created and working

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post, Comment, Profile
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,3 +10,8 @@ def index(request):
     # Shows all the posts
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+# Post views
+def post_detail(request,slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'post_detail.html', {'post':post})
