7b7e36bccd319c6fd6e034057c0a272e484c34c0
jpsantostefano <jpsantostefano@gmail.com>
2024-02-26 11:58:13 +0000

    POst, Comment and Profile models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    type = models.TextField()
+    description = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+
+    def __self__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+
+    class Meta:
+        ordering = ['date_added']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    instagram = models.CharField()
+    email = models.EmailField()
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.user.username
+
+
+# Create Profile when a new user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
